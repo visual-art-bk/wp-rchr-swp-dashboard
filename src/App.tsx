57c4767f73bfa539886f85cc8a64e2b16c748ead
kbk <kekekj@naver.com>
2024-10-07 13:01:29 +0900

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React, { ReactNode } from "react";
 import { createRoot } from "react-dom/client";
-
+import "./app.module.css";
+import Dashboard from "./Dashboard/Dashboard";
 const ROOT_ID_TO_WORDPRESS_BODY_CONTENT = "wpbody-content";
 const rootID = "wpRchrSwpDashboardRoot";
 const rootElement = document.getElementById(rootID);
-
-function App({ isValidRootElement }: { isValidRootElement: boolean }) {
-  if (isValidRootElement === false) {
-    return <div>대시보드 준비중이에요.</div>;
-  }
-  return <div className="app"></div>;
-}
 
 if (rootElement !== null && rootElement !== undefined) {
   createRoot(rootElement).render(<App isValidRootElement={true}></App>);
@@ -23,6 +17,17 @@ if (rootElement !== null && rootElement !== undefined) {
   );
 
   // @ts-ignore
-  // That' why rootWpBodyContent is got by the id of Wordpress. 
+  // That' why rootWpBodyContent is got by the id of Wordpress.
   createRoot(rootWpBodyContent).render(<div>대시보드 준비중이에요.</div>);
+}
+
+function App({ isValidRootElement }: { isValidRootElement: boolean }) {
+  if (isValidRootElement === false) {
+    return <div>대시보드 준비중이에요.</div>;
+  }
+  return (
+    <div className="app" prefix="_wpRchrSwpApp-fd-23">
+      <Dashboard></Dashboard>
+    </div>
+  );
 }
---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,4 @@
+import "./dashboard.module.css";
+export default function Dashboard() {
+  return <div className="wp-rhr-swp-dashboard" prefix="_dash-er-32"></div>;
+}
